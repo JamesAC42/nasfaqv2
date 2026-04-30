7f09280c4d490c62a80fde19d08ad028f854d307
James Crovo <jamescrovo450@gmail.com>
2026-04-30 17:01:57 -0400

    add analytics

--- app-client/app/layout.tsx
+++ app-client/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.scss";
 import { AppProviders } from "@/app/providers/app-providers";
 
+import Script from "next/script";
+
 export const metadata: Metadata = {
   title: "NASFAQ",
   description: "VTuber Numbers",
@@ -15,6 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script
+          src="https://umami.fukuin.dev/script.js"
+          data-website-id="1aaf939c-cd8e-4e9d-bef7-cb4739440bae"
+          strategy="afterInteractive"
+        />
         <AppProviders>{children}</AppProviders>
       </body>
     </html>
